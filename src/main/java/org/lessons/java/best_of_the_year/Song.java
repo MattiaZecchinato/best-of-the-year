f18946891e504cc6d608e2b45925f31918a44879
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-08-04 15:06:01 +0200

    feat: create class Song

---
+++ src/main/java/org/lessons/java/best_of_the_year/Song.java
@@ -0,0 +1,22 @@
+package org.lessons.java.best_of_the_year;
+
+public class Song {
+    private int id;
+    private String title;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
