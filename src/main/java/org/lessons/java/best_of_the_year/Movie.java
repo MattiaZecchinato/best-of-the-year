063fde92923667c67e1611740514b93bcab6d07d
MattiaZecchinato <mattiazecchinato04@gmail.com>
2025-08-04 15:24:19 +0200

    feat: add constructors to Movie and Song classes

--- src/main/java/org/lessons/java/best_of_the_year/Movie.java
+++ src/main/java/org/lessons/java/best_of_the_year/Movie.java
@@ -4,6 +4,15 @@ public class Movie {
     private int id;
     private String title;
 
+    public Movie() {
+
+    }
+
+    public Movie(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
     public int getId() {
         return this.id;
     }
--- src/main/java/org/lessons/java/best_of_the_year/Song.java
+++ src/main/java/org/lessons/java/best_of_the_year/Song.java
@@ -4,6 +4,15 @@ public class Song {
     private int id;
     private String title;
 
+    public Song() {
+
+    }
+
+    public Song(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
     public int getId() {
         return this.id;
     }
